e1eab3f9c113a59934a1080bd2845d5087605772
Michelle Mounde <mitchmounde@gmail.com>
2022-12-10 17:16:39 +0300

    feat: render art image tiles

---
+++ src/components/ArtImageTile/index.js
@@ -0,0 +1,11 @@
+import { Link, useParams } from 'react-router-dom';
+
+function ArtImageTile({ art, galleryId }) {
+  return (
+    <Link to={`/galleries/${galleryId}/art/${art.id}`}>
+      <img src={`${art.images[0].baseimageurl}`} alt={`${art.title}`}/>
+    </Link>
+  )
+}
+
+export default ArtImageTile;
--- src/components/GalleryView/index.js
+++ src/components/GalleryView/index.js
@@ -1,14 +1,25 @@
-import { useParams } from "react-router-dom";
+import { useParams, Route, useRouteMatch } from "react-router-dom";
+import ArtImageTile from "../ArtImageTile";
 
 function GalleryView({ galleries }) {
   const { galleryId } = useParams();
 
   const galleryChoice = galleries.find(gallery => gallery.galleryid === parseInt(galleryId));
 
+  const { url } = useRouteMatch();
+
   return (
     <div className="page-wrapper">
       <h1>Hello from GalleryView</h1>
       <h2>{`${galleryChoice.name}`}</h2>
+      <Route exact path={`${url}`}>
+        {galleryChoice.objects.map(artwork => {
+          return (
+            <ArtImageTile key={artwork.id} art={artwork} galleryId={galleryId}/>
+          )
+        })
+        }
+      </Route>
     </div>
   )
 }
